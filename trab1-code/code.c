9a3ff628f994c4ab17750b4b28d4bb99359b1ae8
Otavio Brito <69211663+otaviofbrito@users.noreply.github.com>
2023-05-03 13:52:51 -0300

    bruteforce size adjustment

--- trab1-code/code.c
+++ trab1-code/code.c
@@ -2,80 +2,95 @@
 #include <stdio.h>
 #include <math.h>
 
-typedef struct {
+typedef struct
+{
     unsigned int peso;
     int beneficio;
 } type_item;
 
-
 int max(int a, int b) { return (a > b) ? a : b; }
 
-
-
-//dinamica bottom-up
-int algoritmo_1(int capacidade, type_item items[], int n) {
+// dinamica bottom-up
+int algoritmo_1(int capacidade, type_item items[], int n)
+{
     int i, w;
-    int K[n+1][capacidade+1];
+    int K[n + 1][capacidade + 1];
 
-    for(i=0; i<=n;i++){
-        for(w = 0; w <= capacidade; w++){
-            if(i==0 || w==0){
+    for (i = 0; i <= n; i++)
+    {
+        for (w = 0; w <= capacidade; w++)
+        {
+            if (i == 0 || w == 0)
+            {
                 K[i][w] = 0;
-            }else if(items[i-1].peso <= w){
-                K[i][w] = max(items[i-1].beneficio + K[i-1][w - items[i-1].peso], K[i-1][w]);
-            }else {
-                K[i][w] = K[i-1][w];
+            }
+            else if (items[i - 1].peso <= w)
+            {
+                K[i][w] = max(items[i - 1].beneficio + K[i - 1][w - items[i - 1].peso], K[i - 1][w]);
+            }
+            else
+            {
+                K[i][w] = K[i - 1][w];
             }
         }
     }
     return K[n][capacidade];
- }
+}
 
-
-int algoritmo_2(int capacidade, type_item items[], int n, int val) {
-    int tempPeso;
-    int tempBeneficio;
+int algoritmo_2(int capacidade, type_item items[], int n)
+{
     int bestPeso = 0;
     int bestBenefico = 0;
-    int *ar = (int *) calloc(capacidade, sizeof(int));
-    int j,k;
-    for(int i = 1; i< pow(2,n); i++){
-        j = n;
-        tempPeso = 0;
-        tempBeneficio = 0;
+    int *ar = (int *)calloc(capacidade, sizeof(int));
+    int j;
+    for (int i = 0; i < pow(2, n); i++)
+    {
+        int j = n-1;
+        int tempPeso = 0;
+        int tempBeneficio = 0;
+        int k;
 
-        while(ar[j] != 0 && j>0){
+        while(ar[j] != 0 && j >0)
+        {
             ar[j] = 0;
             j--;
         }
+
         ar[j] = 1;
-        for(k = 0; k < n; k++){
-            if(ar[k]==1){
+
+        for (k = 0; k < n; k++)
+        {
+            if (ar[k] == 1)
+            {
                 tempPeso = tempPeso + items[k].peso;
                 tempBeneficio = tempBeneficio + items[k].beneficio;
             }
         }
-        if((tempBeneficio > bestBenefico) && (tempPeso <= capacidade)){
+        if ((tempBeneficio > bestBenefico) && (tempPeso <= capacidade))
+        {
             bestBenefico = tempBeneficio;
-            bestPeso = tempPeso;
         }
     }
+    free(ar);
     return bestBenefico;
- }
+}
 
+void algoritmo_3(void) {}
 
-void algoritmo_3(void) { }
-
-void imprimir_items(type_item *items, int n) {
+void imprimir_items(type_item *items, int n)
+{
     int i;
-    for (i = 0; i < n; i++) {
+    for (i = 0; i < n; i++)
+    {
         printf("Peso: %d, Beneficio: %d\n", items[i].peso, items[i].beneficio);
     }
 }
 
-type_item *ler_items(char *filename, int *n_items, int *capacidad) {
+type_item *ler_items(char *filename, int *n_items, int *capacidad)
+{
     FILE *fp = fopen(filename, "r");
-    if (fp == NULL) {
+    if (fp == NULL)
+    {
         puts("Não foi possível abrir o arquivo");
         exit(0);
     }
@@ -85,7 +100,8 @@ type_item *ler_items(char *filename, int *n_items, int *capacidad) {
 
     type_item *items = (type_item *)malloc(*n_items * sizeof(type_item));
 
-    for (int i = 0; i < *n_items; i++) {
+    for (int i = 0; i < *n_items; i++)
+    {
         fscanf(fp, "%d %d", &items[i].peso, &items[i].beneficio);
     }
 
@@ -94,27 +110,34 @@ type_item *ler_items(char *filename, int *n_items, int *capacidad) {
     return items;
 }
 
-
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     int n_items, capacidad;
 
-    if (argc != 3) {
+    if (argc != 3)
+    {
         printf("Uso: %s <nome do arquivo de entrada> <algoritmo>\n", argv[0]);
-        return 1;  
+        return 1;
     }
 
     type_item *items = ler_items(argv[1], &n_items, &capacidad);
     imprimir_items(items, n_items);
 
-    if (argv[2] == "1") {
+    if (argv[2] == "1")
+    {
         printf("teste");
-       printf("%d", algoritmo_1(capacidad, items, n_items) );
-    } else if (argv[2] == "2") {
-        algoritmo_2(capacidad, items, n_items, 0);
-    } else if (argv[2] == "3") {
+    }
+    else if (argv[2] == "2")
+    {
+        printf("test22e");
+    }
+    else if (argv[2] == "3")
+    {
         algoritmo_3();
-    } else {
-        printf("%d\n", algoritmo_1(capacidad, items, n_items));
+    }
+    else
+    {
+        printf("%d\n", algoritmo_2(capacidad, items, n_items));
         return 1;
     }
     free(items);
