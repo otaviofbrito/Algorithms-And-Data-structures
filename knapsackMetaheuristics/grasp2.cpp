cbeacd894e6634d8ecbe92dea7efb8d09ec01859
Otavio Brito <otaviofbrito@gmail.com>
2023-07-04 21:14:00 -0300

    local_search problem

--- knapsackMetaheuristics/grasp2.cpp
+++ knapsackMetaheuristics/grasp2.cpp
@@ -15,12 +15,44 @@ using namespace std;
 
 // task: improve time complexity (greedy randomized)
 
-
 bool compare(const Item obj1, const Item obj2)
 {
   return obj1.ratio > obj2.ratio;
 }
 
+Mochila local_search(Mochila ks)
+{
+
+  Mochila temp = ks;
+
+  for (int r = ks.itens.size() - 1; r > 0; r--)
+  {
+
+    if (temp.itens.at(r).position == 0)
+    {
+      temp.itens.at(r).position = 1;
+      temp.total += temp.itens.at(r).beneficio;
+      temp.capacidade -= temp.itens.at(r).peso;
+    }
+    else
+    {
+      temp.itens.at(r).position = 0;
+      temp.total -= temp.itens.at(r).beneficio;
+      temp.capacidade += temp.itens.at(r).peso;
+    }
+
+    if (ks.total < temp.total && temp.capacidade   )
+    {
+      return temp;
+    }
+    else
+    {
+      temp = ks;
+    }
+  }
+
+  return ks;
+}
 
 Mochila perturbate(Mochila ks, int rate)
 {
@@ -46,7 +78,7 @@ Mochila perturbate(Mochila ks, int rate)
   }
 
   return ks;
-}
+  }
 
 Mochila Iterated_Local_Search(Mochila ks, int itr)
 {
@@ -55,6 +87,7 @@ Mochila Iterated_Local_Search(Mochila ks, int itr)
   for (int i = 0; i < itr; i++)
   {
     temp = perturbate(ks, r);
+    temp = local_search(temp);
     if (ks.total < temp.total && temp.capacidade >= 0)
     {
       return temp;
@@ -68,19 +101,23 @@ Mochila Greedy_Randomized_Construction(Mochila ks, float alfa)
 {
   mt19937 rng(std::random_device{}());
 
-  vector<Item> temp_itens = ks.itens;
+  vector<Item *> temp_itens;
+  for (int j = 0; j < ks.itens.size(); j++)
+  {
+    temp_itens.push_back(&ks.itens.at(j));
+  }
 
   while (!temp_itens.empty())
   {
-    double c_min = temp_itens.back().ratio;
-    double c_max = temp_itens.front().ratio;
+    double c_min = temp_itens.back()->ratio;
+    double c_max = temp_itens.front()->ratio;
 
     // Create RCL
-    vector<Item> RCL;
+    vector<Item *> RCL;
     for (int i = 0; i < temp_itens.size(); i++)
     {
-      Item item = temp_itens.at(i);
-      double c = item.ratio;
+      Item *item = temp_itens.at(i);
+      double c = item->ratio;
       if (c >= c_min + alfa * (c_max - c_min))
       {
         RCL.push_back(item);
@@ -89,39 +126,33 @@ Mochila Greedy_Randomized_Construction(Mochila ks, float alfa)
 
     uniform_int_distribution<int> distribution(0, RCL.size() - 1);
     int r_index = distribution(rng);
-    Item r_item = RCL.at(r_index);
+    Item *r_item = RCL.at(r_index);
 
-    if (ks.capacidade - r_item.peso >= 0 && r_item.position == 0)
+    if (ks.capacidade - r_item->peso >= 0 && r_item->position == 0)
     {
-      ks.capacidade -= r_item.peso;
-      ks.total += r_item.beneficio;
-      for (int j = 0; j < ks.itens.size(); j++)
-      {
-        if (ks.itens.at(j).id == r_item.id)
-        {
-          ks.itens.at(j).position = 1;
-        }
-      }
+      ks.capacidade -= r_item->peso;
+      ks.total += r_item->beneficio;
+      r_item->position = 1;
     }
 
-    auto itr = std::remove_if(temp_itens.begin(), temp_itens.end(), [&](Item a)
-                              { return a.id == RCL.at(r_index).id; });
+    auto itr = std::remove_if(temp_itens.begin(), temp_itens.end(), [&](Item *a)
+                              { return a->id == RCL.at(r_index)->id; });
 
     temp_itens.erase(itr, temp_itens.end());
   }
   return ks;
 }
 
-Mochila grasp(Mochila ks, float alfa, int itr, int ils_itr)
+Mochila grasp(Mochila ks, float alfa, int GRASPmax, int ILSmax)
 {
   sort(ks.itens.begin(), ks.itens.end(), compare);
   Mochila solution = ks;
   Mochila temp_solution;
 
-  for (int k = 0; k < itr; k++)
+  for (int k = 0; k < GRASPmax; k++)
   {
     temp_solution = Greedy_Randomized_Construction(ks, alfa);
-    temp_solution = Iterated_Local_Search(temp_solution, ils_itr);
+    temp_solution = Iterated_Local_Search(temp_solution, ILSmax);
     if (temp_solution.total > solution.total)
     {
       solution = temp_solution;
@@ -130,7 +161,6 @@ Mochila grasp(Mochila ks, float alfa, int itr, int ils_itr)
 
   return solution;
 }
-
 
 int main(int argc, char const *argv[])
 {
@@ -144,7 +174,7 @@ int main(int argc, char const *argv[])
 
   start = clock();
   // grasp(knapsack, alfa, graspMaxIteration, ilsMaxIteration)
-  knapsack = grasp(knapsack, 0.95, 5, 20);
+  knapsack = grasp(knapsack, 0.9, 5, 1);
 
   end = clock();
 
